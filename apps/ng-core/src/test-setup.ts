78be91b134000eef76ce70d2d954439a82c63016
Jitendra Behera <jb.jitu@live.com>
2026-04-07 11:10:29 +0530

    vitest

--- apps/ng-core/src/test-setup.ts
+++ apps/ng-core/src/test-setup.ts
@@ -1,10 +1,7 @@
-// @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
-globalThis.ngJest = {
-  testEnvironmentOptions: {
-    errorOnUnknownElements: true,
-    errorOnUnknownProperties: true,
-  },
-};
-import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+import '@angular/compiler';
+import '@analogjs/vitest-angular/setup-zone';
+import { setupTestBed } from '@analogjs/vitest-angular/setup-testbed';
 
-setupZoneTestEnv();
+setupTestBed({
+  zoneless: false,
+});
---
+++ apps/ng-core/vitest.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import angular from '@analogjs/vite-plugin-angular';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig(() => ({
+  plugins: [angular(), nxViteTsPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test-setup.ts'],
+    include: ['src/**/*.{test,spec}.ts'],
+    reporters: ['default'],
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: '../../coverage/apps/ng-core',
+    },
+  },
+}));
